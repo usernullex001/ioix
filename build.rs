4a0027a2e234b34606763ed7aa5bebb220d88601
nullex <bsmlflill@outlook.com>
2026-08-10 06:13:16 +0330

    cargo fmtttt :DDDD

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
- println!("cargo:rerun-if-changed=info.json");
- let v:serde_json::Value=serde_json::from_str(&std::fs::read_to_string("info.json").unwrap()).unwrap();
- println!("cargo:rustc-env=version={}",v["ver"].as_str().unwrap());
+    println!("cargo:rerun-if-changed=info.json");
+    let v: serde_json::Value =
+        serde_json::from_str(&std::fs::read_to_string("info.json").unwrap()).unwrap();
+    println!("cargo:rustc-env=version={}", v["ver"].as_str().unwrap());
 }
